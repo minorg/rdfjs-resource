0462fe4a3a1bb3e4a85dc8a2c4fc5ff30ca063ba
Minor Gordon <minorg@server.fake>
2026-02-25 12:47:02 -0500

    LiteralDecoder tests

--- src/vocabularies.ts
+++ src/vocabularies.ts
@@ -43,6 +43,9 @@ export namespace xsd {
   export const float = DataFactory.namedNode(
     "http://www.w3.org/2001/XMLSchema#float",
   );
+  export const int = DataFactory.namedNode(
+    "http://www.w3.org/2001/XMLSchema#int",
+  );
   export const integer = DataFactory.namedNode(
     "http://www.w3.org/2001/XMLSchema#integer",
   );
